5aef754df9bfd856b5c5021ff089f93bee8f2d93
acaycedo <afcaycedo14@gmail.com>
2021-09-24 19:58:08 -0500

    Esto no era Equis DE
    Eliminado FirstProyect

--- BO/firstproyect/src/clasetienda/Leche.java
+++
@@ -1,7 +0,0 @@
-
-package clasetienda;
-
-
-public class Leche {
-    
-}
--- BO/firstproyect/src/interfaces/SerVivo.java
+++
@@ -1,8 +0,0 @@
-
-package interfaces;
-
-public interface SerVivo {
-    public abstract String dormir ();
-    public abstract String comer ();
-    public abstract String reproducirse ();
-}
